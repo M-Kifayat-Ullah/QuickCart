3e52c9e2bbe16040b9f990fdaba73a635117bff9
M-Kifayat-Ullah <mkifayat005@gmail.com>
2024-11-18 06:51:57 +0500

    customer data

---
+++ Customer.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Customer_id { get; set; }
+        public string Customer_name { get; set; }
+        public string Customer_email { get; set; }
+        public string Customer_country { get; set; }
+        public string Customer_image { get; set; }
+        public string Customer_password { get; set; }
+        public string Customer_city { get; set; }
+        public string Customer_phone { get; set; }
+        public string Customer_address { get; set; }
+        public string Customer_gender { get; set; }
+    }
+}
